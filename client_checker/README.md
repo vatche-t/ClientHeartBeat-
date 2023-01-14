476ce3a1892c23e4bf69c2c72d60340c36ab6d77
vatche-t <vatche.thorossian@gmail.com>
2023-01-14 09:23:09 +0000

    added email notifier

---
+++ client_checker/README.md
@@ -0,0 +1,6 @@
+# ClientHeartBeat
+
+
+*using smtp service for email notifier.
+*add two step authentication @google account to add app passwords 
+
--- client_checker/function.py
+++ client_checker/function.py
@@ -5,6 +5,29 @@ import datetime as dt
 
 
 
+
+import smtplib
+import ssl
+from email.message import EmailMessage
+
+email_sender = '[your email address]'
+email_password = '[your password]'
+email_receiver = '[recipient email]'
+
+
+subject = 'Client has been disconnected for to long!!'
+body = """
+Your client has been disconnected for more than 20 minutes.
+"""
+
+em = EmailMessage()
+em['From'] = email_sender
+em['To'] = email_receiver
+em['Subject'] = subject
+em.set_content(body)
+
+context = ssl.create_default_context()
+
 def time_stamp():
     with open('..//127.0.0.1.last', 'rb')as file:
         fc = file.read()
@@ -16,7 +39,9 @@ present = datetime.now()
 
 diff = (present - time_stamp() ).total_seconds() 
 
-if diff > 10 :
-    print('The client has been disconnected for more than 10  seconds.')
+if diff > 60 :
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+        smtp.login(email_sender, email_password)
+        smtp.sendmail(email_sender, email_receiver, em.as_string())
 else:
     print("client is connected....")
